888acd16c33b1bacd14579d2a60a355452602b0f
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2024-04-09 12:21:00 -0700

    test

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+x = [3,6,3,2,5,7,9]
+print(x[6])
+
+y = {
+    "logan" : 3,
+    "kevin" : 2,
+    "tobias" : 4,
+    "haruto" : 2,
+    "tyler" : 6
+}
+
+print(y["tyler"])
---
+++ sample.py
@@ -0,0 +1,24 @@
+
+
+class rectangle:
+    length = 0
+    width = 0
+
+    def perimeter(self):
+        p = self.length + self.width + self.length + self.width
+        return p
+
+    def area(self):
+        pass
+
+    def __init__(self,a,b):
+        self.length = a
+        self.width = b
+
+
+
+r1 = rectangle(3,4)
+r2 = rectangle(5,6)
+
+print(r1.length)
+print(r1.perimeter() )
